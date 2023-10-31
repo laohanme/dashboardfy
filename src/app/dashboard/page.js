28fb7ec969beafb60f0e0bab7c6ea842863750d7
laohan <jimocheng97@hotmail.com>
2023-10-31 15:12:13 +0800

    add dashboard page

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+
+
+export default function DashboardPage() {
+    return (
+        <div className="flex justify-center flex-col items-center h-screen">
+            <p>DASHBOARD</p>
+        </div>
+    )
+}
