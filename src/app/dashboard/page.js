ffd82c69d43595f9aed38fa19f992510f00ef296
laohan <jimocheng97@hotmail.com>
2023-10-31 15:19:24 +0800

    add line

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,6 +3,7 @@
 export default function DashboardPage() {
     return (
         <div className="flex justify-center flex-col items-center h-screen">
+            <p>i dont know</p>
         </div>
     )
 }
